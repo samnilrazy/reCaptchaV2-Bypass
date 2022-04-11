ea5378affbd736a46e85dd81c290a77dc8356f70
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-11 10:20:44 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -1,45 +1,30 @@
-# open the file
-import speech_recognition as sr 
-import os 
-import subprocess
 from selenium import webdriver
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
 import time
-import pyautogui
 
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+def scrape_top_news():
+
+    browser = webdriver.Chrome(
+        executable_path="/Users/akjasim/chromedriver/chromedriver")
+
+    wait = WebDriverWait(browser, 10)
+    browser.get('https://news.ycombinator.com/')
+    element_list = wait.until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".title > a"))
+    )
+    for element in element_list:
+        try:
+            title, url = element.text, element.get_attribute('href')
+            print("Title:", title, "\nURL:", url, end="\n\n")
+        except Exception as e:
+            print(e)
+    time.sleep(2)
+    browser.quit()
 
 
-filename = "payload.wav"
-r = sr.Recognizer()
-
-
-inkk = 'https://convertio.co/pt/mp3-wav/'
-
-nav = webdriver.Chrome()
-nav.get(inkk)
-
-nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[1]/div[1]/label').click()
-#nav.find_element_by_id("Enviar").click()
-time.sleep(3)
-pyautogui.write('payload.mp3') 
-pyautogui.press('enter')
-time.sleep(2)
-nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div/div[2]/div[2]/button').click()
-time.sleep(15)
-nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div[2]/div/div[1]/table/tbody/tr/td[6]/a').click()
-print("OK")
-
-with sr.AudioFile(filename) as source:
-    # listen for the data (load audio to memory)
-    audio_data = r.record(source)
-    # recognize (convert from speech to text)
-    text = r.recognize_google(audio_data)
-    print("O Conteudo é: {}".format(text))
+if __name__ == '__main__':
+    scrape_top_news()
