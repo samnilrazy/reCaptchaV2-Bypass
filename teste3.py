156306a19b78b4dc59ef3b56965f4a2ef44b19c3
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-11 17:42:16 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -1,19 +1,26 @@
-from pydub import AudioSegment
-import speech_recognition as sr 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# files                                                                       
-src = "payload.mp3"
-dst = "wav.wav"
-
-# convert wav to mp3                                                            
-audSeg = AudioSegment.from_mp3("payload.mp3")
-audSeg.export(dst, format="wav")
-filename = 'wav.wav'
-
-r = sr.Recognizer()
-with sr.AudioFile(filename) as source:
-    # listen for the data (load audio to memory)
-    audio_data = r.record(source)
-    # recognize (convert from speech to text)
-    text = r.recognize_google(audio_data)
-    print("O Conteudo é: {}".format(text))
+options = webdriver.ChromeOptions() 
+options.add_argument("start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.google.com/recaptcha/api2/demo")
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor']")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
+time.sleep(3) 
+frames = driver.find_elements_by_tag_name("iframe")
+recaptcha_control_frame = None
+recaptcha_challenge_frame = None
+driver.switch_to.default_content()
+frames = driver.find_elements_by_tag_name("iframe")
+driver.switch_to.frame(recaptcha_challenge_frame)
+    
+# click on audio challenge
+time.sleep(5)
+driver.find_element_by_id("recaptcha-audio-button").click()
+print("Concluído")
