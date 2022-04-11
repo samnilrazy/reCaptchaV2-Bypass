f198225cf6f4eed851d0be137bb02a427540cc7c
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-11 09:55:07 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -1,6 +1,37 @@
-import os
+# open the file
+import speech_recognition as sr 
+import os 
+import subprocess
+from selenium import webdriver
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 import time
-while 1==1:
-  cwd = os.getcwd()
-  print(cwd)
-  time.sleep(10)
+import pyautogui
+
+
+filename = "payload.wav"
+r = sr.Recognizer()
+
+
+inkk = 'https://convertio.co/pt/mp3-wav/'
+
+nav = webdriver.Chrome()
+nav.get(inkk)
+
+nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[1]/div[1]/label').click()
+#nav.find_element_by_id("Enviar").click()
+time.sleep(3)
+pyautogui.write('payload.mp3') 
+pyautogui.press('enter')
+time.sleep(2)
+nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div/div[2]/div[2]/button').click()
+time.sleep(15)
+nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div[2]/div/div[1]/table/tbody/tr/td[6]/a').click()
+print("OK")
+
+with sr.AudioFile(filename) as source:
+    # listen for the data (load audio to memory)
+    audio_data = r.record(source)
+    # recognize (convert from speech to text)
+    text = r.recognize_google(audio_data)
+    print("O Conteudo é: {}".format(text))
