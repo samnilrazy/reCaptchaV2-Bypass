87ab51835e6d003df037efe042f8de8117bea20c
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-11 11:59:24 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -1,6 +1,14 @@
-import os
+# open the file
+import speech_recognition as sr 
+import os 
+import subprocess
 from selenium import webdriver
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import time
+import pyautogui
 
+# ===============================================================
 op = webdriver.ChromeOptions()
 op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 op.add_argument("--headless")
@@ -8,9 +16,33 @@ op.add_argument("--no-sandbox")
 op.add_argument("--disable-dev-sh-usage")
 
 
-driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+nav = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+# ===============================================================
 
-driver.get("https://youtube.com")
+filename = "payload.wav"
+r = sr.Recognizer()
 
-print(driver.page_source)
-print("Concluído")
+
+inkk = 'https://convertio.co/pt/mp3-wav/'
+
+
+#nav = webdriver.Chrome()
+nav.get(inkk)
+
+nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div/div[2]/div[1]/div[1]/label').click()
+#nav.find_element_by_id("Enviar").click()
+time.sleep(3)
+pyautogui.write('payload.mp3') 
+pyautogui.press('enter')
+time.sleep(2)
+nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[3]/div[2]/div/div[2]/div[2]/div/div[2]/div[2]/button').click()
+time.sleep(15)
+nav.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div[2]/div/div[1]/table/tbody/tr/td[6]/a').click()
+print("OK")
+
+with sr.AudioFile(filename) as source:
+    # listen for the data (load audio to memory)
+    audio_data = r.record(source)
+    # recognize (convert from speech to text)
+    text = r.recognize_google(audio_data)
+    print("O Conteudo é: {}".format(text))
