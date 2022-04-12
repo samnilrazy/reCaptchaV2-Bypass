047186439862dbb87bd0bbb894542525e5c50b27
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-11 23:00:18 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -15,6 +15,7 @@ from pydub import AudioSegment
 options = webdriver.ChromeOptions() 
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 options.add_argument("--headless")
+options.add_argument("window-size=1400,800")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-sh-usage")
 options.add_argument("--disable-blink-features")
