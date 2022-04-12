6fe29b6fc5ffad584f021173275582d5e542eb54
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-12 14:31:55 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -75,14 +75,14 @@ time.sleep(3)
 #recaptcha_challenge_frame = None
 
 driver.switch_to.frame(recaptcha_control_frame)
-driver.switch_to.default_content()
+#driver.switch_to.default_content()
 
 
 #WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[starts-with(@src, 'https://www.recaptcha.net/recaptcha/api2/bframe')]")))
 WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"/html/body/div[5]/div[4]/iframe"))) #localiza iframe
 
-framee = driver.find_element(By.XPATH, value='/html/body/div[5]/div[4]/iframe')
-driver.switch_to_frame(frame) #localiza iframe
+#framee = driver.find_element(By.XPATH, value='/html/body/div[5]/div[4]/iframe')
+#driver.switch_to_frame(frame) #localiza iframe
                        
 print("Passou da terceira etapa")
 time.sleep(2)
