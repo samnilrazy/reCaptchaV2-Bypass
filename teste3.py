ee6febbab8084463e34a85a5572c65a607d94f38
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-12 10:04:35 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -70,7 +70,7 @@ time.sleep(3)
 #driver.switch_to.default_content()
 
 
-WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[starts-with(@src, 'https://www.recaptcha.net/recaptcha/api2/bframe')]"))))
+WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[starts-with(@src, 'https://www.recaptcha.net/recaptcha/api2/bframe')]")))
 WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID,'recaptcha-audio-button')))
 #frames = driver.find_elements_by_tag_name("iframe")
 #driver.switch_to.frame(recaptcha_challenge_frame)
