aa7c5f0a3e777c5e408aea061216092839bb0f4f
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-11 22:20:56 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -2,25 +2,97 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import urllib.request
+import speech_recognition as sr 
 import time
+from pydub import AudioSegment
+
+
+
+#driver = webdriver.Chrome(ChromeDriverManager().install()) 
 
 options = webdriver.ChromeOptions() 
+options.add_argument("--disable-blink-features")
+options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument("start-maximized")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
+#options.add_argument("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1")
+options.add_argument('--disable-notifications')
+options.add_argument("--mute-audio")
+
+
+
+PROXY = '159.203.84.241:3128'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument(f'--proxy-server{PROXY}')
+
 driver = webdriver.Chrome(options=options)
-driver.get("https://www.google.com/recaptcha/api2/demo")
-WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor']")))
+
+#driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+
+
+driver.get("https://iir.ai/OZkS")
+
+recaptcha_control_frame = None
+
+
+
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.recaptcha.net/recaptcha/api2/anchor']")))
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
 time.sleep(3) 
-frames = driver.find_elements_by_tag_name("iframe")
-recaptcha_control_frame = None
-recaptcha_challenge_frame = None
+#frames = driver.find_elements_by_tag_name("iframe")
+#recaptcha_control_frame = None
+#recaptcha_challenge_frame = None
+
+driver.switch_to.frame(recaptcha_control_frame)
 driver.switch_to.default_content()
-frames = driver.find_elements_by_tag_name("iframe")
-driver.switch_to.frame(recaptcha_challenge_frame)
-    
-# click on audio challenge
-time.sleep(5)
-driver.find_element_by_id("recaptcha-audio-button").click()
-print("Concluído")
+
+
+
+#frames = driver.find_elements_by_tag_name("iframe")
+#driver.switch_to.frame(recaptcha_challenge_frame)
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"/html/body/div[4]/div[4]/iframe")))
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-audio-button"]'))).click()
+time.sleep(1)
+
+driver.switch_to.frame(recaptcha_control_frame)
+driver.switch_to.default_content()
+
+# //*[@id="audio-source"]
+# //*[@id=":2"] = play
+# /html/body/div/div/div[7]/a = download
+WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"/html/body/div[4]/div[4]/iframe")))
+src = driver.find_element_by_id("audio-source").get_attribute("src")
+src = str(src)
+print("O link é: {}".format(src))
+time.sleep(1)
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[7]/a'))).click()
+
+
+
+urllib.request.urlretrieve(src, "ex.mp3")
+time.sleep(1)
+filename = "audio.wav"
+r = sr.Recognizer()
+
+
+
+# files                                                                       
+src = "ex.mp3"
+dst = "audio.wav"
+
+# convert wav to mp3                                                            
+audSeg = AudioSegment.from_mp3(src)
+audSeg.export(dst, format="wav")
+time.sleep(1)
+
+
+with sr.AudioFile(filename) as source:
+    # listen for the data (load audio to memory)
+    audio_data = r.record(source)
+    # recognize (convert from speech to text)
+    text = r.recognize_google(audio_data)
+    print("O Conteudo é: {}".format(text))
