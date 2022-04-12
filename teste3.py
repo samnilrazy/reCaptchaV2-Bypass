98a3dd5e5564343f6e7217d051d29ca8e0f51366
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-11 23:10:12 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -47,7 +47,7 @@ recaptcha_control_frame = None
 
 
 
-WebDriverWait(nav, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[starts-with(@src, 'https://www.recaptcha.net/recaptcha/api2/anchor')]")))
+WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[starts-with(@src, 'https://www.recaptcha.net/recaptcha/api2/anchor')]")))
 WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
 time.sleep(3) 
 #frames = driver.find_elements_by_tag_name("iframe")
