2d11f13102111cd7ca6648f4a01665d6b610e09d
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-13 11:46:08 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -75,7 +75,7 @@ time.sleep(5)
 #frames2 = driver.find_element(By.NAME, "reCaptcha")
 #print("2_Os frames são: {}".format(frames2))
 
-driver.switch_to.frame(frames[4])
+driver.switch_to.frame(frames[-1])
 #driver.find_element(By.XPATH, value='//*[@id="recaptcha-anchor"]/div[1]').click
 driver.find_element(By.ID, 'recaptcha-anchor').click
 
