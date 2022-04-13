70dea8268db1c6b6d5100e20e3e07fec6238fc0f
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-12 22:37:25 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -95,11 +95,12 @@ print("Passou da terceira etapa(q era localizar xpath)")
 
 time.sleep(4)
 print("pequeno teste")
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-audio-button"]'))).click()
 #driver.find_element(By.ID, 'recaptcha-audio-button').click()
-driver.find_element(By.XPATH, value='//*[@id="recaptcha-audio-button"]').click()
+#driver.find_element(By.XPATH, value='//*[@id="recaptcha-audio-button"]').click()
 print("passou do pequeno teste")
 
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-audio-button"]'))).click()
+
 
 #WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//*[@id="recaptcha-audio-button"]'))).click() # localiza e clica no fone
 #driver.find_element(By.XPATH, '//*[@id="recaptcha-audio-button"]').click()
