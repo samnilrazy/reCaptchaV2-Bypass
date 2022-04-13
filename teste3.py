4dce480fa97d18e40a8cab03c98e3bd4c2b1f39b
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-13 11:08:41 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -75,8 +75,9 @@ time.sleep(5)
 frames2 = driver.find_elements(By.XPATH, '/html/body/div[5]/div[4]/iframe')
 print("2_Os frames são: {}".format(frames2))
 
-#driver.switch_to.frame(frames[1])
-driver.find_element(By.XPATH, value='//*[@id="recaptcha-anchor"]/div[1]').click
+driver.switch_to.frame(frames[3])
+#driver.find_element(By.XPATH, value='//*[@id="recaptcha-anchor"]/div[1]').click
+driver.find_element(By.ID, 'recaptcha-anchor').click
 
 driver.switch_to.default_content()
 
