13209381a383a8c4e072a098a39fe5470ecc05fd
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-13 13:35:45 -0400

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -90,7 +90,7 @@ print("clicou na caixa")
 driver.switch_to.default_content()
 
 driver.find_elements(By.TAG_NAME, 'iframe')
-driver.switch_to.frame(frames[3])
+driver.switch_to.frame(frames[-1])
 
 print("Passou da segunda etapa")
 #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
