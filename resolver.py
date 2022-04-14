fa8a27ef975c90f246153c8c7a7a42ef6f0a8fd8
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-13 22:09:31 -0400

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,56 +1,33 @@
+from anticaptchaofficial.recaptchav2proxyless import *
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-import urllib.request
-import speech_recognition as sr 
-import time
 import os
-from pydub import AudioSegment
-from selenium.webdriver.chrome.service import Service
+import time
 print("Iniciando...")
-
-
-
 s = Service('/app/.chromedriver/bin/chromedriver')
-
 
 driver = webdriver.Chrome(service=s)
 
-driver.get("https://www.google.com/recaptcha/api2/demo")
+url = "https://www.google.com/recaptcha/api2/demo"
+page = driver.get(url)
 
+time.sleep(10)
 
-frames = driver.find_elements(By.TAG_NAME, "iframe")
+frames = driver.find_elements(By.TAG_NAME, 'iframe')
 driver.switch_to.frame(frames[0])
-time.sleep(4)
-
-driver.find_element(By.CLASS_NAME, "recaptcha-checkbox-border").click()
 
 
-driver.switch_to.default_content()
-
-frames = driver.find_element(By.XPATH, "/html/body/div[2]/div[4]").find_elements(By.TAG_NAME, "iframe")
-time.sleep(4)
+sitekey = driver.find_element(By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]').click()
 
 driver.switch_to.default_content()
+time.sleep(10)
 
-frames = driver.find_elements(By.TAG_NAME, "iframe")
+frames = driver.find_elements(By.TAG_NAME, 'iframe')
+driver.switch_to.frame(frames[2])
 
-driver.switch_to.frame(frames[-1])
-
-frames = driver.find_elements(By.ID, "recaptcha-audio-button").click()
-
-driver.switch_to.default_content()
-
-frames = driver.find_elements(By.TAG_NAME, "iframe")
-
-driver.switch_to.frame(frames[-1])
-
-time.sleep(4) #/html/body/div/div/div[3]/div/button
-
-driver.find_elements(By.XPATH, "/html/body/div/div/div[3]/div/button")
-print("concluido")
+sitekey = driver.find_element(By.XPATH, '//*[@id="recaptcha-audio-button"]').click()
+time.sleep(10)
 
 
-
-
+print("OK")
