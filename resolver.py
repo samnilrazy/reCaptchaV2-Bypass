1ca67669d5dde4ea5fa086bef3326c34e6d86535
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-13 22:35:53 -0400

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,5 +1,6 @@
 #from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import os
 import time
