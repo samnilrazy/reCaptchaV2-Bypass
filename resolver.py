3cf53bb54ae7d8583e785bf390f34349cb258543
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-13 22:48:23 -0400

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import speech_recognition as sr 
 import os
 from pydub import AudioSegment
 import urllib.request
