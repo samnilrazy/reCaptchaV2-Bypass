053316b334b373345bca569771b42abd63c06010
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-14 10:46:17 -0400

    Update resolver2.py

--- resolver2.py
+++ resolver2.py
@@ -59,7 +59,9 @@ print("achou o frame {}".format(tpp))
 driver.switch_to.frame(tpp)
 
 
-sitekey = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div[1]/div/div/span/div[1]').click()
+#sitekey = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div[1]/div/div/span/div[1]').click()
+botao = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]')))
+botao.click()
 
 driver.switch_to.default_content()
 time.sleep(3)
