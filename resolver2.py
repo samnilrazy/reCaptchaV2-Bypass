51046d48b08997046b0aa62bc6a9490f1f81f74e
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-14 09:53:13 -0400

    Update resolver2.py

--- resolver2.py
+++ resolver2.py
@@ -1,5 +1,6 @@
 #from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import speech_recognition as sr 
