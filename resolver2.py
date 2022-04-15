329249117f5f5875667678cba1f45ddb220b2ecd
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-14 22:53:37 -0400

    Update resolver2.py

--- resolver2.py
+++ resolver2.py
@@ -70,7 +70,7 @@ print(len(frames))
 tpp = WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[starts-with(@src, 'https://www.recaptcha.net/recaptcha/api2/anchor')]")))
 print("achou o frame {}".format(tpp))
 #driver.switch_to.frame(frames[3])
-driver.switch_to.frame(tpp)
+#driver.switch_to.frame(tpp)
 time.sleep(10)
 state = "N"
 count = 0
