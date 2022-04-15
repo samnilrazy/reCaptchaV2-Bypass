5736d185beeeedcef8186c2fa9c70036ca86e50b
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-15 12:07:51 -0400

    Update resolver2.py

--- resolver2.py
+++ resolver2.py
@@ -68,7 +68,7 @@ print(len(frames))
 
 #WebDriverWait(driver, 50).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div/div[2]/div[2]/form/div[2]/div/div/div/iframe')))
 #tpp = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div/div[2]/div[2]/form/div[2]/div/div/div/iframe')))
-tpp = WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//*[@id="captchaShortlink"]/div/div/iframe')))
+#tpp = WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//*[@id="captchaShortlink"]/div/div/iframe')))
 print("achou o frame {}".format(tpp))
 #driver.switch_to.frame(frames[3])
 tpp = "a-i7xirmh7o0z"
