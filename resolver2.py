8d3d416a26e1e3858f3092919a8c50c0762b576c
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-15 11:51:31 -0400

    Update resolver2.py

--- resolver2.py
+++ resolver2.py
@@ -84,7 +84,7 @@ except:
 try:
     #botao = driver.find_element(By.XPATH, '//*[@id="recaptcha-anchor"]')
     #botao = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="recaptcha-anchor"]')))
-    botao = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]')))
+    botao = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'recaptcha-anchor')))
     state = "Ok"
     #driver.switch_to.default_content()
 except Exception as e:
@@ -95,7 +95,7 @@ except Exception as e:
         time.sleep(10)
         try:
             #botao = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '//*[@id="recaptcha-anchor"]')))
-            botao = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]')))
+            botao = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'recaptcha-anchor')))
             state = "Ok"
         except:
             time.sleep(1)
