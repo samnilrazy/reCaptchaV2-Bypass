ebcd1d312df9a6c4cb63803d3ef88b6c80b207ce
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-15 23:13:34 -0400

    Update resolver2.py

--- resolver2.py
+++ resolver2.py
@@ -150,6 +150,7 @@ try:
 except:
      print("Close não encontrado")
      time.sleep(2)
+WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]'))).click() # <----------------- REMOVER
 
 #WebDriverWait(driver, 50).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div/div[2]/div[2]/form/div[2]/div/div/div/iframe')))
 #tpp = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/div/div/div/div[2]/div[2]/form/div[2]/div/div/div/iframe'))).get_attribute("name")
