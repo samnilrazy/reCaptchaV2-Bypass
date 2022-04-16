b62fdc76f9a5eee57b57664060327cb3f7ffa9e6
Sam Nilrazy <samnilrazy@gmail.com>
2022-04-16 12:39:01 -0400

    Update resolver2.py

--- resolver2.py
+++ resolver2.py
@@ -176,7 +176,8 @@ try: # tentar achar no captcha box
     #botao = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]')))
     #botao = driver.find_element(By.XPATH, '//*[@id="recaptcha-anchor"]/div[1]')
     #botao = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#captchaShortlink > div > div > iframe')))
-    botao = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.ID, 'recaptcha-anchor')))
+    #botao = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.ID, 'recaptcha-anchor')))
+    botao = driver.find_element(By.XPATH, value='//*[@id="captchaShortlink"]/div/div/iframe').click()
     state = "Ok"
     #driver.switch_to.default_content()
 except:
